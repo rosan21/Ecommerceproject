129789452c763a01844536f9bf68aa8b3b597ae0
roshan <roshangurung125@gmail.com>
2022-07-04 22:02:28 +0545

    Cart Model, CartProduct Model, AddCartViews

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -35,20 +35,41 @@ class ProductDetailView(TemplateView):
         return context
 
 class AddToCartView(TemplateView):
-    template_name ='addtocart.html'
+    template_name = 'addtocart.html'
 
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
-    	#get product id
         product_id = self.kwargs['id']
+        product_object = Product.objects.get(id=product_id)
 
-        #get product
-        product = Product.objects.get(id = product_id)
-
+        cart_id = self.request.session.get('cart_id',None )
         #check if cart exists
-        card_id = self.request.session.get('cart_id', None)
-        
-        
+        if cart_id:
+            cart_object = Cart.objects.get(id=cart_id)
+            this_product_in_cart = cart_object.cartproduct_set.filter(product=product_object)
+            #checking if the product already exists in the cart and if it exists
+            if this_product_in_cart.exists():
+                cartproduct = this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal +=product_object.selling_price
+                cartproduct.save()
+                cart_object.total += product_object.selling_price
+                cart_object.save()
+            #new product added to cart
+            else:
+                cartproduct= CartProduct.objects.create(cart=cart_object, product=product_object,rate=product_object.selling_price,quantity=1,subtotal=product_object.selling_price)
+                cart_object.total += product_object.selling_price
+                cart_object.save()
+        #if cart doesnot exist
+        else:
+            cart_object = Cart.objects.create(total=0)
+            self.request.session['cart_id'] = cart_object.id
+            cartproduct= CartProduct.objects.create(cart=cart_object, product=product_object,rate=product_object.selling_price,quantity=1,subtotal=product_object.selling_price)
+            cart_object.total += product_object.selling_price
+            cart_object.save()
+
+
+
 
 
         return context
